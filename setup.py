bc706b3ac5bf31e99640552ab35ce9f89a1bd95a
niceanyh <niceanyh@gmail.com>
2022-11-27 13:03:04 +0000

    v1.0.0

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='niceanyh2focus',
     version='1.0.0',
+    author = "Niceanyh",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
